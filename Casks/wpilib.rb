19754c3da3f5f8b1ac6e7390ca25a446e5d9e67e
Noam Zaks <noamzaks@outlook.com>
2021-03-02 09:57:06 +0200

    Update WPILib cask

--- Casks/wpilib.rb
+++ Casks/wpilib.rb
@@ -1,25 +1,31 @@
 cask 'wpilib' do
-  version '2020.3.2'
-  sha256 'e1aa02d7026c923d4b2c8f32c5ec1872f7ef903ca27652af279f79d19cbd5f15'
+  version '2021.2.2'
+  sha256 'ad09828f8283fae96c7f74a1bef85f9ac36a7811224977c2027b8eddb82b04d9'
 
   # github.com/wpilibsuite/allwpilib was verified as official when first introduced to the cask
-  url "https://github.com/wpilibsuite/allwpilib/releases/download/v#{version}/WPILib_Mac-#{version}.tar.gz"
+  url "https://github.com/wpilibsuite/allwpilib/releases/download/v#{version}/WPILib_macOS-#{version}.dmg"
   appcast 'https://github.com/wpilibsuite/allwpilib/releases.atom'
   name 'WPILib Suite'
   homepage 'https://wpilib.org/'
 
-  depends_on cask: 'visual-studio-code'
+  depends_on  cask: 'visual-studio-code'
 
-  install_dir = "#{ENV['HOME']}/wpilib/2020"
+  install_dir = "#{ENV['HOME']}/wpilib/#{version.split('.', 1)[0]}"
 
-  artifact '.', target: install_dir
+  artifact 'artifacts', target: install_dir
 
   preflight do
-    system_command '/usr/bin/python', args: ["#{staged_path}/tools/ToolsUpdater.py"]
+    system_command 'mkdir', args: ['-p', "#{staged_path}/artifacts"]
+    system_command 'tar', args: ['-zxf', "#{staged_path}/WPILib_Mac-#{version}-artifacts.tar.gz", '-C', "/usr/local/Caskroom/wpilib/#{version}/artifacts"]
 
-    Dir.glob("#{staged_path}/vsCodeExtensions/*.vsix") do |vsix_file|
+    system_command '/usr/bin/python', args: ["#{staged_path}/artifacts/tools/ToolsUpdater.py"]
+
+    Dir.glob("#{staged_path}/artifacts/vsCodeExtensions/*.vsix") do |vsix_file|
       system_command '/usr/local/bin/code', args: ['--install-extension', vsix_file]
     end
+
+    system_command 'rm', args: ["#{staged_path}/WPILib_Mac-#{version}-artifacts.tar.gz"]
+    system_command 'rm', args: ['-rf', "#{staged_path}/WPILibInstaller.app"]
   end
 
   uninstall_preflight do
